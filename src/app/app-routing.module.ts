0c6bdeea8de945103accbade455052045e65c71f
Adolfo Ruiz <ruizruiz.adolfo@gmail.com>
2024-01-22 17:30:16 +0100

    404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { CourseComponent } from './pages/course/course.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { UserComponent } from './pages/user/user.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { SlugResolver } from './resolver/slug.resolver';
 
 const routes: Routes = [
@@ -11,7 +12,7 @@ const routes: Routes = [
   { path: ':slug/course/:id', component: CourseComponent, resolve: { schoolData: SlugResolver } },
   { path: ':slug/cart', component: CartComponent, resolve: { schoolData: SlugResolver } },
   { path: ':slug/user', component: UserComponent, resolve: { schoolData: SlugResolver } },
-  { path: '**', redirectTo: '' },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 const routerOptions: ExtraOptions = {
