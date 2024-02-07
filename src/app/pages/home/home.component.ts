660ba3bda5f9882972f1f235dd69d79d1f53de17
ad17427 <andoni.serrano@versia.com>
2024-02-07 18:02:47 +0100

    Filter by degree order empty

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -55,7 +55,7 @@ export class HomeComponent implements OnInit {
   /** Filters**/
   selectedDegreeType: number;
   degreeValues: any = {
-    doesntMatter: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    doesntMatter: null,
     novice: [1, 2, 3],
     intermediate: [4, 5, 6],
     advanced: [7, 8, 9],
