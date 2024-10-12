f2c954d3aca2786b3e42904c5b5a085027412e3a
Hulala <yang.ye.1@hotmail.com>
2024-10-12 22:42:01 +0200

    Ajuste pantalla front principal

--- src/app/components/course-card/app.component.ts
+++ src/app/components/course-card/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home-course-card',
@@ -8,5 +9,99 @@ import { Component, Input } from '@angular/core';
 export class CourseCardComponent {
   @Input() data: any
   Week: string[] = ["dom", "lun", "mar", "mie", "jue", "vie", "sab"]
-  getWeekDay(v: string) { return new Date(v).getDay() }
+  WeekDisplayed: number[] = []
+  constructor(public translateService: TranslateService) { }
+  getWeekDay(): string {
+    const uniqueDays: Set<number> = new Set();
+    this.data.course_dates.forEach((item: any) => {
+      const day = new Date(item.date).getDay();
+      uniqueDays.add(day);
+    });
+    const dayNames: string[] = Array.from(uniqueDays).map(day => this.Week[day]);
+    if (dayNames.length === 0) return "";
+    if (dayNames.length === 1) return dayNames[0];
+    const lastDay = dayNames.pop();
+    return dayNames.join(", ") + " y " + lastDay;
+  }
+
+  findMaxHourEnd(): string {
+    const maxHourStart = Math.max(
+      ...this.data.course_dates.map((date: any) => {
+        return parseInt(date.hour_end.replace(":", ""));
+      })
+    );
+    const maxHourString = maxHourStart.toString().padStart(4, "0");
+    return `${maxHourString.slice(0, 2)}:${maxHourString.slice(2)}`;
+  }
+  findMinHourStart(): string {
+    const maxHourStart = Math.max(
+      ...this.data.course_dates.map((date: any) => {
+        return parseInt(date.hour_start.replace(":", ""));
+      })
+    );
+    const minHourString = maxHourStart.toString().padStart(4, "0");
+    return `${minHourString.slice(0, 2)}:${minHourString.slice(2)}`;
+  }
+
+
+  getShotrDescription() {
+    if (!this.data.translations || this.data.translations === null) {
+      return this.data.short_description;
+    } else {
+      const translations = JSON.parse(this.data.translations);
+      return translations[this.translateService.currentLang].short_description;
+    }
+  }
+
+  getDescription() {
+    if (!this.data.translations || this.data.translations === null) {
+      return this.data.description;
+    } else {
+      const translations = JSON.parse(this.data.translations);
+      return translations[this.translateService.currentLang].description;
+    }
+  }
+
+  getCourseName() {
+    if (!this.data.translations || this.data.translations === null) {
+      return this.data.name;
+    } else {
+      const translations = JSON.parse(this.data.translations);
+      return translations[this.translateService.currentLang].name;
+    }
+  }
+
+  getCoursePrice() {
+    if (this.data.course_type == 2 && this.data.is_flexible) {
+      const priceRange = this.data.price_range.find((a: any) => a[1] !== null);
+      return priceRange[1];
+    } else {
+      return this.data.price
+    }
+  }
+
+  getWeekdays(settings: string): string {
+    const settingsObj = JSON.parse(settings);
+    const weekDays = settingsObj.weekDays;
+    const daysMap: any = {
+      "monday": "Lundi",
+      "tuesday": "Mardi",
+      "wednesday": "Mercredi",
+      "thursday": "Jeudi",
+      "friday": "Vendredi",
+      "saturday": "Samedi",
+      "sunday": "Diamanche",
+    };
+    const activeDays = Object.entries(weekDays)
+      .filter(([_, isActive]) => isActive)
+      .map(([day]) => this.translateService.instant(daysMap[day]));
+
+    if (activeDays.length === 7) {
+      return `${this.translateService.instant('Lundi')} - ${this.translateService.instant('Diamanche')}`;
+    } else {
+      return activeDays.join(', ');
+    }
+  }
+
+  protected readonly JSON = JSON;
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -32,14 +32,7 @@ export class HomeComponent implements OnInit {
   schoolData: any = null;
   sports: any[];
 
-  fullText: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at eros tempor, sollicitudin sem sit amet, ornare augue. Cras eget neque fermentum, rutrum dolor at, vulputate odio. Duis nec pulvinar eros. Ut et interdum ante. Nulla id quam lectus. In efficitur congue nisi, vel dapibus felis egestas sed.';
-  displayedText: string;
-  displayedTextOld: string;
-  showSeeMore: boolean = false;
-  showSeeLess: boolean = false;
-  private maxLength: number = 100;
 
-  /** Filters**/
   selectedDegreeType: number;
   degreeValues: any = {
     doesntMatter: null,
@@ -63,15 +56,12 @@ export class HomeComponent implements OnInit {
     { id: 5, label: 'text_adults' }
   ];
   currentDegreeRange: number[] = [];
-  selectedSportId: number;
   selectedCourseType: number;
   degreesSports: any;
-  selectedAgeType: number;
   min_age: number;
   max_age: number;
   daySelected: any;
   userLogged: any;
-  defaultImage = '../../../assets/images/3.png';
 
   constructor(private router: Router, public themeService: ThemeService, private coursesService: CoursesService, public translateService: TranslateService,
     private schoolService: SchoolService, private datePipe: DatePipe, private authService: AuthService,
@@ -80,7 +70,6 @@ export class HomeComponent implements OnInit {
 
 
   ngOnInit(): void {
-    //  this.userLogged = JSON.parse(localStorage.getItem(this.authService.slug+ '-boukiiUser') ?? '');
     this.FormGroup = this.fb.group({
       deporte: ["", Validators.required],
       cursoType: ["", Validators.required],
@@ -92,16 +81,8 @@ export class HomeComponent implements OnInit {
       data => {
         if (data) {
           this.schoolData = data.data;
-          this.selectedAgeType = parseInt(localStorage.getItem(this.schoolData.slug + '-selectedAgeType') ?? '1');
-          this.selectedDegreeType = parseInt(localStorage.getItem(this.schoolData.slug + '-selectedDegreeType') ?? '1');
-          this.selectedCourseType = parseInt(localStorage.getItem(this.schoolData.slug + '-selectedCourseType') ?? '1');
-
           this.setAgeRange();
-          if (this.schoolData?.sports?.length > 0) {
-            this.selectedSportId =
-              parseInt(localStorage.getItem(this.schoolData.slug + '-selectedSportId') ?? this.schoolData.sports[0].id);
-            this.getCourses();
-          }
+          this.getCourses();
         }
       }
     );
@@ -111,190 +92,87 @@ export class HomeComponent implements OnInit {
         this.userLogged = data;
       }
     );
-
-    //SEE MORE -> do it for each course received
-    if (this.fullText.length > this.maxLength) {
-      this.displayedText = this.fullText.substring(0, this.maxLength) + '...';
-      this.displayedTextOld = this.fullText.substring(0, this.maxLength) + '...';
-      this.showSeeMore = true;
-    } else {
-      this.displayedText = this.fullText;
-    }
-  }
-
-  reloadFilters() {
-    this.setAgeRange();
-    this.setDegreeRange();
-    localStorage.setItem(this.schoolData.slug + '-selectedCourseType', this.selectedCourseType.toString())
-    localStorage.setItem(this.schoolData.slug + '-selectedSportId', this.selectedSportId.toString())
-    this.getCourses();
-
   }
 
   getCourses() {
     let params = {
-      'start_date': this.formatDate(2024, 1, 1),
+      'start_date': this.formatDate(2000, 1, 1),
       'end_date': this.formatDate(2034, 1, 1),
-      //'course_type': this.selectedCourseType,
-      //'degree_order': this.currentDegreeRange,
-      //'sport_id': this.selectedSportId,
-      //'max_age': this.max_age,
-      //'min_age': this.min_age
     };
     this.coursesService.getCoursesAvailableByDates(params)
       .subscribe(res => {
-        this.ResultCourse = res.data;
         this.DestacadoCourse = res.data;
         this.ProximosCourse = res.data;
       });
   }
-
-  showFullText() {
-    this.displayedText = this.fullText;
-    this.showSeeMore = false;
-    this.showSeeLess = true;
+  searchCourses() {
+    this.setDegreeRange()
+    this.setAgeRange()
+    let params = {
+      'start_date': this.formatDate(2000, 1, 1),
+      'end_date': this.formatDate(2034, 1, 1),
+      'course_type': this.FormGroup.controls['nivel'].value,
+      'degree_order': this.currentDegreeRange,
+      'sport_id': this.FormGroup.controls['deporte'].value,
+      'max_age': this.max_age,
+      'min_age': this.min_age
+    };
+    this.coursesService.getCoursesAvailableByDates(params).subscribe(res => this.ResultCourse = res.data)
   }
-
-  showLessText() {
-    this.displayedText = this.displayedTextOld;
-    this.showSeeMore = true;
-    this.showSeeLess = false;
-  }
-
-
 
   formatDate(year: number, month: number, day: number): string {
     return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
   }
 
-  goTo(url: string) {
-    this.router.navigate([url]);
-  }
-
   setAgeRange(): void {
-    switch (this.selectedAgeType) {
-      case 1: // Todas las edades
+    switch (this.FormGroup.controls['edat'].value) {
+      case 1:
         this.min_age = 1;
-        this.max_age = 99; // Poner un valor alto para 'todas las edades'
+        this.max_age = 99;
         break;
-      case 2: // 2 a 3
+      case 2:
         this.min_age = 2;
         this.max_age = 3;
         break;
-      case 3: // 3 a 5
+      case 3:
         this.min_age = 3;
         this.max_age = 5;
         break;
-      case 4: // Más de 5
+      case 4:
         this.min_age = 6;
-        this.max_age = 18; // O el valor máximo que consideres apropiado
+        this.max_age = 18;
         break;
-      case 5: // Solo adultos +18
+      case 5:
         this.min_age = 18;
-        this.max_age = 99; // O el valor máximo que consideres apropiado
+        this.max_age = 99;
         break;
       default:
         this.min_age = 1;
         this.max_age = 99;
         break;
     }
-    localStorage.setItem(this.schoolData.slug + '-selectedAgeType', this.selectedAgeType.toString())
   }
 
   setDegreeRange(): void {
-    switch (this.selectedDegreeType) {
-      case 1: // No importa
+    switch (this.FormGroup.controls['nivel'].value) {
+      case 1:
         this.currentDegreeRange = this.degreeValues.doesntMatter;
         break;
-      case 2: // Principiante
+      case 2:
         this.currentDegreeRange = this.degreeValues.novice;
         break;
-      case 3: // Intermedio
+      case 3:
         this.currentDegreeRange = this.degreeValues.intermediate;
         break;
-      case 4: // Avanzado
+      case 4:
         this.currentDegreeRange = this.degreeValues.advanced;
         break;
-      case 5: // Experto
+      case 5:
         this.currentDegreeRange = this.degreeValues.expert;
         break;
       default:
         this.currentDegreeRange = [];
         break;
     }
-    localStorage.setItem(this.schoolData.slug + '-selectedDegreeType', this.selectedDegreeType.toString())
   }
-
-  getShotrDescription(course: any) {
-
-    if (!course.translations || course.translations === null) {
-      return course.short_description;
-    } else {
-      const translations = JSON.parse(course.translations);
-      return translations[this.translateService.currentLang].short_description;
-    }
-  }
-
-  getDescription(course: any) {
-
-    if (course) {
-      if (!course.translations || course.translations === null) {
-        return course.description;
-      } else {
-        const translations = JSON.parse(course.translations);
-        return translations[this.translateService.currentLang].description;
-      }
-    }
-
-  }
-
-  getCourseName(course: any) {
-    if (course) {
-      if (!course.translations || course.translations === null) {
-        return course.name;
-      } else {
-        const translations = JSON.parse(course.translations);
-        return translations[this.translateService.currentLang].name;
-      }
-    }
-  }
-
-  getCoursePrice(course: any) {
-    if (course) {
-      if (course.course_type == 2 && course.is_flexible) {
-        const priceRange = course.price_range.find((a: any) => a[1] !== null);
-        return priceRange[1];
-      } else {
-        return course.price
-      }
-    }
-
-    return 0;
-  }
-
-  getWeekdays(settings: string): string {
-    const settingsObj = JSON.parse(settings);
-    const weekDays = settingsObj.weekDays;
-    const daysMap: any = {
-      "monday": "Lundi",
-      "tuesday": "Mardi",
-      "wednesday": "Mercredi",
-      "thursday": "Jeudi",
-      "friday": "Vendredi",
-      "saturday": "Samedi",
-      "sunday": "Diamanche",
-    };
-
-    const activeDays = Object.entries(weekDays)
-      .filter(([_, isActive]) => isActive)
-      .map(([day]) => this.translateService.instant(daysMap[day]));
-
-    if (activeDays.length === 7) {
-      return `${this.translateService.instant('Lundi')} - ${this.translateService.instant('Diamanche')}`;
-    } else {
-      return activeDays.join(', ');
-    }
-  }
-
-  protected readonly JSON = JSON;
 }
