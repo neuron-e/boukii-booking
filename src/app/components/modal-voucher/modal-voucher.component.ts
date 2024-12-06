b11fccba6b49eb2d3241e60323b3c7a881c90c9c
Hulala <yang.ye.1@hotmail.com>
2024-12-06 15:05:54 +0100

    Quitar console

--- src/app/components/modal-voucher/modal-voucher.component.ts
+++ src/app/components/modal-voucher/modal-voucher.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
 import { ThemeService } from '../../services/theme.service';
-import {ClientService} from '../../services/client.service';
+import { ClientService } from '../../services/client.service';
 
 @Component({
   selector: 'app-modal-voucher',
@@ -25,7 +25,7 @@ export class ModalVoucherComponent implements OnInit {
   @Input() slug: string;
   @Output() onClose = new EventEmitter<any>();
 
-  code:string;
+  code: string;
 
   constructor(public themeService: ThemeService, private clientService: ClientService) { }
 
@@ -34,8 +34,8 @@ export class ModalVoucherComponent implements OnInit {
   }
 
   searchVoucher() {
-    let storageSlug = localStorage.getItem(this.slug+ '-boukiiUser');
-    if(storageSlug) {
+    let storageSlug = localStorage.getItem(this.slug + '-boukiiUser');
+    if (storageSlug) {
       let userLogged = JSON.parse(storageSlug);
       this.clientService.getClientVoucher(this.code, userLogged.clients[0].id).subscribe(res => {
         this.onClose.emit(res);
